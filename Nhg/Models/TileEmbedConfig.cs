958eba402f749e9c773b98849930e6dfdcca4d4c
Kari Sinivuori <kari.sinivuori@nhg.fi>
2018-08-12 01:05:49 +0300

    Add project files.

---
+++ Nhg/Models/TileEmbedConfig.cs
@@ -0,0 +1,10 @@
+using Microsoft.PowerBI.Api.V2.Models;
+using System;
+
+namespace Nhg.Models
+{
+    public class TileEmbedConfig : EmbedConfig
+    {
+        public string dashboardId { get; set; }
+    }
+}
